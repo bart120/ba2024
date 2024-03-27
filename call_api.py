f505f985db57c6ff095b8ca887448320754f88c6
Vincent Leclerc <vleclerc@inow.fr>
2024-03-27 12:26:43 +0100

    dpe

--- call_api.py
+++ call_api.py
@@ -2,8 +2,16 @@ import requests
 
 url_root = "https://koumoul.com/data-fair/api/v1/datasets/dpe-france/lines"
 
-reponse = requests.get(url_root)
+code_commune = 95500
+url_api = f'{url_root}?qs=code_insee_commune_actualise:"{code_commune}"'
+#url_api = url_root + '?qs=code_insee_commune_actualise:"' + str(code_commune)+'"'
+reponse = requests.get(url_api)
 
 contenu = reponse.json()
 
 print(contenu)
+
+
+
+# python -m pip install requests
+#https://pythononlinecompiler.com/
